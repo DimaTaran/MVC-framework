a00d9794dcffdeea9cee18af7dc4d508ada854dd
Dima Taran <taran_da@ukr.net>
2020-07-24 18:36:11 +0300

    change contollers

--- src/Controller/FooController.php
+++ src/Controller/FooController.php
@@ -3,8 +3,22 @@
 
 namespace Taran\Controller;
 
+//use Symfony\Component\HttpFoundation\Request;
+//use Symfony\Component\HttpFoundation\Response;
+
+use Nyholm\Psr7\Response;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\ResponseInterface;
 
 class FooController
 {
+    /**
+     * @param ServerRequestInterface $request
+     * @return ResponseInterface
+     */
+    public function run(ServerRequestInterface $request, array $arg): ResponseInterface
+    {
+        return new Response(200, [], "FOO page ". htmlspecialchars($arg['name']), 1.1);
+    }
 
 }
--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -3,8 +3,24 @@
 
 namespace Taran\Controller;
 
+//use Symfony\Component\HttpFoundation\Request;
+//use Symfony\Component\HttpFoundation\Response;
+
+use Nyholm\Psr7\Response;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\ResponseInterface;
+
 
 class IndexController
 {
+    /**
+     * @param ServerRequestInterface $request
+     * @return ResponseInterface
+     */
+
+    public function run(ServerRequestInterface $request): ResponseInterface
+    {
+        return new Response(200, [], "Index Page", 1.1);
+    }
 
 }
