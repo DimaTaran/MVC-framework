01220484ecd9e945e2161f885cb78c6013c39f0f
Dima Taran <taran_da@ukr.net>
2020-07-24 18:24:30 +0300

    add middleware package

--- public/index.php
+++ public/index.php
@@ -1,13 +1,67 @@
 <?php
-
 declare(strict_types = 1);
-use Symfony\Component\HttpFoundation\Response;
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
 
-require_once __DIR__ . 'vendor/autoload.php';
+
+
+use Symfony\Component\Routing\Loader\YamlFileLoader;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\Routing\Router;
+use Taran\App;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$appRoot = dirname(__DIR__);
+
+$router = new Router(
+    new YamlFileLoader(new FileLocator([$appRoot . '/etc'])),
+    'routes.yml',
+    ['cache_dir' => $appRoot . '/var/cache']
+);
 
 $request = \Symfony\Component\HttpFoundation\Request::createFromGlobals();
+$response = (new App($appRoot, $router))->handle($request);
+$response->send();
 
 
+//
+//$request = $httpFactory->createRequest($symfonyRequest);
+//$router->setContext((new RequestContext())->fromRequest($symfonyRequest));
+
+//try {
+//    $handlerMeta = $router->matchRequest($symfonyRequest);
+////var_dump($handlerMeta);
+//    /** @var string $controller */
+//    $controller = $handlerMeta['_controller'];
+//
+//    /** @var string $action */
+//    $action = $handlerMeta['_action'];
+//
+//    $response = (new $controller)->$action($request, $handlerMeta);
+//
+//} catch (ResourceNotFoundException $e) {
+//    $response = $psr17Factory->createResponse(404, 'Handler not found');
+//
+//} catch (Exception $e) {
+//    $response = $psr17Factory->createResponse(500, 'Internal server error');
+//}
+//(new HttpFoundationFactory())->createResponse($response)->send();
+
+//if ($symfonyRequest->get('page') === 'foo') {
+//    $response = (new FooController())->run($request);
+////    $response = new Response("FOO page");
+//
+//} else {
+//    $response = (new IndexController())->run($request);
+////    $response = new Response("Index page");
+//}
+
+//var_dump($request);
+//echo $response->getBody();
+
+//
 //if (isset($_GET['page']) && $_GET['page'] === 'foo' ) {
 //   echo "FOO page";
 //} else echo "Index page";
--- src/App.php
+++ src/App.php
@@ -3,8 +3,51 @@
 
 namespace Taran;
 
+use Exception;
+use Symfony\Component\HttpFoundation\Response;
+use Nyholm\Psr7\Factory\Psr17Factory;
+use Symfony\Bridge\PsrHttpMessage\Factory\HttpFoundationFactory;
+use Symfony\Bridge\PsrHttpMessage\Factory\PsrHttpFactory;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
+use Symfony\Component\Routing\RequestContext;
+use Symfony\Component\Routing\Router;
 
 class App
 {
+    public function  __construct(string $appRoot, Router $router)
+    {
+        $this->appRoot = $appRoot;
+        $this->router = $router;
+        $psr17Factory = new Psr17Factory();
+        $this->httpFactory = new PsrHttpFactory($psr17Factory, $psr17Factory, $psr17Factory, $psr17Factory);
+
+    }
+
+    public function handle(Request $request): Response
+    {
+        $this->router->setContext((new RequestContext())->fromRequest($request));
+
+        try {
+            $handlerMeta = $this->router->matchRequest($request);
+
+            /** @var string $controller */
+            $controller = $handlerMeta['_controller'];
+
+            /** @var string $action */
+            $action = $handlerMeta['_action'];
+
+            $psrResponse = (new $controller)->$action($this->httpFactory->createRequest($request), $handlerMeta);
+            $response = (new HttpFoundationFactory())->createResponse($psrResponse);
+
+        } catch (ResourceNotFoundException $e) {
+            $response = new Response('Handler not found', Response::HTTP_NOT_FOUND);
+
+        } catch (Exception $e) {
+            $response = new Response('Internal server error', Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+        return $response;
+
+    }
 
 }
